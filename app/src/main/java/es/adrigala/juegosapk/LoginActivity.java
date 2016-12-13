b06812514c876febfe068153e0afa90bb73d26fb
galaterro <adrix1992@gmail.com>
2016-12-13 18:08:34 +0100

    Initial commit

---
+++ app/src/main/java/es/adrigala/juegosapk/LoginActivity.java
@@ -0,0 +1,25 @@
+package es.adrigala.juegosapk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText etNombre;
+    private EditText etPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        etNombre = (EditText) findViewById(R.id.textNombre);
+        etPass = (EditText) findViewById(R.id.textPass);
+
+    }
+
+    public void entrarLogin(View view) {
+        
+    }
+}
---
+++ app/src/main/java/es/adrigala/juegosapk/MainActivity.java
@@ -0,0 +1,22 @@
+package es.adrigala.juegosapk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void cambiarPantalla(View view) {
+        Toast t = Toast.makeText(this, "pedos", Toast.LENGTH_LONG);
+        t.show();
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+    }
+}
